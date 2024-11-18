ec40ff42cd36c9a9b1a8231c27717e4d9173bb6e
Ali <ali.abolfathee@yahoo.com>
2024-11-18 17:26:13 +0330

    added new class by ali

--- model/entity/__init__.py
+++ model/entity/__init__.py
@@ -3,5 +3,7 @@ from model.entity.department import Department
 from model.entity.organization import Organization
 from model.entity.group import Group
 from model.entity.ticket import Ticket
+from model.entity.property import Property
+from model.entity.product_group import ProductGroup
 
-__all__ = ["Person", "Department","Organization","Group","Ticket"]
+__all__ = ["Person", "Department","Organization","Group","Ticket","ProductGroup","Property"]
