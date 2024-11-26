0aee1983987816dbee53a2f484681f3779cb147f
Amirhossein <167887406+AmirsMoradi@users.noreply.github.com>
2024-11-26 11:32:15 +0330

    Update user.py
    update

--- model/entity/user.py
+++ model/entity/user.py
@@ -5,24 +5,28 @@ from model.tools.validator import pattern_validator
 import hashlib
 
 class User(Base):
-    __tablename__ = "user_tbl"
+    tablename = "user_tbl"
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    user_name = Column(String(30), nullable=False, unique=True)
-    password = Column("password", String(128), nullable=False)
-    email = Column(String(50), unique=True)
-    role_id = Column(Integer, ForeignKey("role_tbl.id"), nullable=False)
-    person_id = Column(Integer, ForeignKey("person_tbl.id"), nullable=False)
+    _id = Column("id", Integer, primary_key=True, autoincrement=True)
+    _user_name = Column("user_name", String(30), nullable=False, unique=True)
+    _password = Column("password", String(128), nullable=False)
+    _email = Column("email", String(50), unique=True)
+    _role_id = Column("role_id", Integer, ForeignKey("role_tbl.id"), nullable=False)
+    _person_id = Column("person_id", Integer, ForeignKey("person_tbl.id"), nullable=False)
 
     role = relationship("Role", back_populates="users")
     person = relationship("Person", back_populates="user")
 
-    def __init__(self, user_name, password, email, role_id, person_id):
+    def init(self, user_name, password, email, role_id, person_id):
         self.user_name = user_name
         self.password = password
         self.email = email
         self.role_id = role_id
         self.person_id = person_id
+
+    @property
+    def id(self):
+        return self._id
 
     @property
     def user_name(self):
@@ -51,3 +55,23 @@ class User(Base):
     @pattern_validator(r'^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$', "ایمیل معتبر نیست.")
     def email(self, value):
         self._email = value
+
+    @property
+    def role_id(self):
+        return self._role_id
+
+    @role_id.setter
+    def role_id(self, value):
+        if not isinstance(value, int) or value <= 0:
+            raise ValueError("شناسه نقش باید یک عدد مثبت باشد.")
+        self._role_id = value
+
+    @property
+    def person_id(self):
+        return self._person_id
+
+    @person_id.setter
+    def person_id(self, value):
+        if not isinstance(value, int) or value <= 0:
+            raise ValueError("شناسه شخص باید یک عدد مثبت باشد.")
+        self._person_id = value
