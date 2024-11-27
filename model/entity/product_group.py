85fcb9a6a63ba533e173717ddf11ba0f1549a30f
Ali <ali.abolfathee@yahoo.com>
2024-11-27 16:31:32 +0330

    added by ali

--- model/entity/product_group.py
+++ model/entity/product_group.py
@@ -5,54 +5,27 @@ from model.entity.base import Base
 
 class ProductGroup(Base):
     __tablename__ = "product_group_tbl"
-    _group_code = Column("group_code", Integer, nullable=False,primary_key=True)
-    _product_group_name = Column("product_group_name", String(30), nullable=False)
-    _group_code_up = Column("_group_code_up", Integer, nullable=False)
-    _description = Column("_description", String(200), nullable=False)
+    _id = Column("id", Integer, primary_key=True, nullable=False)
+    _name = Column("name", String(30), nullable=False)
 
-    def __init__(self, group_code, product_group_name, group_code_up, description):
-        self._group_code = group_code
-        self._product_group_name = product_group_name
-        self._group_code_up = group_code_up
-        self._description = description
-
+    def __init__(self, id, name):
+        self._id = id
+        self._name = name
 
     @property
-    def group_code(self):
-        return self._group_code
+    def id(self):
+        return self._id
 
-    @group_code.setter
-    @pattern_validator(r"^[0-9]{2,30}$", " کد گروه معتبر نیست !")
-    def group_code(self, group_code):
-        self._group_code = group_code
-
-    @property
-    def product_group_name(self):
-        return self._product_group_name
-
-    @product_group_name.setter
-    @pattern_validator(r"^[A-Za-z]{2,30}$", "نام گروه کالا معتبر نیست!")
-    def product_group_name(self, product_group_name):
-        self._product_group_name = product_group_name
+    @id.setter
+    @pattern_validator("^[0-9]$", "آیدی معتبر نیست")
+    def id(self, id):
+        self._id = id
 
     @property
-    def group_code_up(self):
-        return self._group_code
+    def name(self):
+        return self._name
 
-    @group_code_up.setter
-    @pattern_validator(r"^[0-9]{2,30}$", " کد گروه بالا دستی معتبر نیست!")
-    def group_code_up(self, group_code_up):
-        self._group_code_up = group_code_up
-
-    @property
-    def description(self):
-        return self._description
-
-    @product_group_name.setter
-    @pattern_validator(r"^[A-Za-z]{2,200}$", " توضیحات معتبر نیست!")
-    def description(self, description):
-        self._description = description
-
-
-
-
+    @name.setter
+    @pattern_validator("^[a-zA-Z]{2,30}$", "نام معتبر نیست")
+    def name(self, name):
+        self._name = name
---
+++ model/entity/unit.py
@@ -0,0 +1,64 @@
+from model.da.data_access import Base
+from model.tools.validator import pattern_validator
+from sqlalchemy import Column, Integer, String
+
+
+class Unit(Base):
+    __tablename__ = "unit_tbl"
+    _id = Column("id", Integer, primary_key=True, autoincrement=True)
+    _name = Column("name", String(30), nullable=False)
+    _amount = Column("amount", Integer, nullable=False)
+    _description = Column("description", String(200), nullable=False)
+
+    def __init__(self, id, name, amount, description):
+        self._id = id
+        self._name = name
+        self._amount = amount
+        self._description = description
+
+    @property
+    def id(self):
+        return self._id
+
+    @id.setter
+    @pattern_validator("^[0-9]$", " آیدی معتبر نیست!")
+    def id(self, id):
+        self._id = id
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str):
+            self._name = name
+        else:
+            raise ValueError("نام معتبر نیست!")
+
+    @property
+    def amount(self):
+        return self._amount
+
+    @amount.setter
+    def amount(self, amount):
+        if isinstance(amount, int):
+            self._amount = amount
+        else:
+            raise ValueError("تعداد معتبر نیست")
+
+    @property
+    def description(self):
+        return self._description
+
+    @description.setter
+    def description(self, description):
+        if isinstance(description, str):
+            self._description = description
+
+        else:
+            raise ValueError("توضیحات معتبر نیست")
+
+
+
+
