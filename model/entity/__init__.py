3ab61125e775755c1849694ce33de2b8985e505b
Ali <ali.abolfathee@yahoo.com>
2024-11-28 15:08:01 +0330

    added by ali

--- model/entity/__init__.py
+++ model/entity/__init__.py
@@ -4,5 +4,14 @@ from model.entity.group import Group
 from model.entity.ticket import Ticket
 from model.entity.product_group import ProductGroup
 from model.entity.inventory_product import InventoryProduct
+from model.entity.Group_property import Group_property
+from model.entity.Product import Product
+from model.entity.Product_Property_Value import Product_Property_Value
+from model.entity.unit import Unit
+from model.entity.person import Person
+from model.entity.Role import Role
+from model.entity.user import User
+from model.entity.permission import Permission
 
-__all__ = ["Person", "Department","Group","Ticket","ProductGroup",]
+__all__ = ["Person", "Department","Group","Ticket","ProductGroup","Product","Product_Property_Value","unit"
+           ,"Role","User","Permission"]
