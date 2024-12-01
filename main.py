96a26f75df35faabb0c926ad822194fbd0f2ff32
Farbod <farbodowrang98@gmail.com>
2024-12-01 11:26:38 +0330

    updated!

--- main.py
+++ main.py
@@ -1,30 +1,34 @@
-from fastapi import FastAPI, Request, Form
-from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
+from flask import Flask, render_template, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_socketio import SocketIO, emit
+from datetime import datetime, timedelta
 from controller.controller import Controller
 
-app = FastAPI()
-templates = Jinja2Templates(directory="view/templates")
-app.mount("/static", StaticFiles(directory="view/static"), name="static")
+app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
 
-@app.get("/", response_class=HTMLResponse)
-async def show_form(request: Request):
-    return templates.TemplateResponse("add_organization.html", {"request": request})
 
-@app.post("/add-organization")
-async def add_organization(
-    name: str = Form(...),
-    slogan: str = Form(...),
-    logo: str = Form(...),
-    duties: str = Form(...),
-    address: str = Form(...),
-    telephone: str = Form(...),
-    description: str = Form(""),
-    head_id: int = Form(None)
-):
-    success, message = Controller.add_organization(name, slogan, logo, duties, address, telephone, description, head_id)
-    if success:
-        return {"message": "سازمان با موفقیت اضافه شد!"}
-    else:
-        return {"error": f"{message}"}
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# @app.post("/add-organization")
+# async def add_organization(
+#     name: str = Form(...),
+#     slogan: str = Form(...),
+#     logo: str = Form(...),
+#     duties: str = Form(...),
+#     address: str = Form(...),
+#     telephone: str = Form(...),
+#     description: str = Form(""),
+#     head_id: int = Form(None)
+# ):
+#     success, message = Controller.add_organization(name, slogan, logo, duties, address, telephone, description, head_id)
+#     if success:
+#         return {"message": "سازمان با موفقیت اضافه شد!"}
+#     else:
+#         return {"error": f"{message}"}
+
+if __name__ == '__main__':
+    socketio.run(app, host="0.0.0.0", port=5001, debug=True)
